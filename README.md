33ab3a791c0bfbd0b2259a6e1b0d4c2eb51d78b1
ChuJiani <1240082967@qq.com>
2022-09-07 01:45:54 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Standard Point Positioning
+Hello SPP!
---
+++ src/SPP.cpp
@@ -0,0 +1,11 @@
+﻿// SPP.cpp : Defines the entry point for the application.
+//
+
+#include "SPP.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello CMake." << endl;
+    return 0;
+}
